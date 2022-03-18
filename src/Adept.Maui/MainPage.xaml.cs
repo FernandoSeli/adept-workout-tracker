d87b2261577b1daa31519c1e1702463bcfb7371f
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-03-19 00:20:16 +0100

    initial commit
    commited base blazor and maui projects

---
+++ src/Adept.Maui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Adept.Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Adept.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Adept.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
