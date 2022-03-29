85c63e99598b9c9dec2e9bc023657d26d5e2e799
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-03-29 21:20:22 +0200

    closes #7

--- src/Adept.Blazor.WASM/Program.cs
+++ src/Adept.Blazor.WASM/Program.cs
@@ -5,6 +5,7 @@ using MudBlazor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,16 +13,28 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 
-builder.Services.AddDbContext<AdeptDatabaseContext>(options => options.UseSqlite($"Filename=app.db"));
-//builder.Services.AddDbContextFactory<AdeptDatabaseContext>(options => options.UseSqlite($"Filename=app.db"));
+//builder.Services.AddDbContext<AdeptDatabaseContext>(options => options.UseSqlite($"Filename=app.db"));
+builder.Services.AddDbContextFactory<AdeptDatabaseContext>(options => options.UseSqlite($"Filename=app.db"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
-//var dbFactory = builder.Services.BuildServiceProvider().GetService<IDbContextFactory<AdeptDatabaseContext>>();
+var dbFactory = builder.Services.BuildServiceProvider().GetService<IDbContextFactory<AdeptDatabaseContext>>();
 
-//var db = dbFactory?.CreateDbContext();
+var db = dbFactory?.CreateDbContext();
 
-//var isCreated = db?.Database.EnsureCreated();
+var isCreated = db?.Database.EnsureCreated();
 
 await builder.Build().RunAsync();
+
+
+public partial class Program
+{
+    /// <summary>
+    ///     FIXME: This is required for EF Core 6.0 as it is not compatible with trimming.
+    ///     See https://github.com/dotnet/efcore/issues/26288 & https://github.com/dotnet/efcore/issues/26860
+    /// </summary>
+    [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
+    // ReSharper disable once UnusedMember.Local
+    private static readonly Type s_keepDateOnly = typeof(DateOnly);
+}
