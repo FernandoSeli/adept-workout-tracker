1937816e2207991a94fb332e518e88bc5bed5bb6
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-04-17 20:27:38 +0200

    Added Exercise Auto complete field #12

---
+++ src/Adept.Blazor/Services/ExerciseService.cs
@@ -0,0 +1,27 @@
+﻿using Adept.Data;
+using Adept.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adept.Blazor.Services
+{
+    public class ExerciseService : IExerciseService
+    {
+        private AdeptDatabaseContext _context;
+
+        public ExerciseService(IDbContextFactory<AdeptDatabaseContext> DbContextFactory)
+        {
+            _context = DbContextFactory.CreateDbContext();
+        }
+
+        public async Task<int> AddExerciseAsync(Exercise exercise)
+        {
+            await _context.Exercises.AddAsync(exercise);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Exercise>> GetExercisesAsync()
+        {
+            return _context.Exercises.AsNoTracking();
+        }
+    }
+}
---
+++ src/Adept.Blazor/Services/IExerciseService.cs
@@ -0,0 +1,10 @@
+﻿using Adept.Data.Model;
+
+namespace Adept.Blazor.Services
+{
+    public interface IExerciseService
+    {
+        Task<IEnumerable<Exercise>> GetExercisesAsync();
+        Task<int> AddExerciseAsync(Exercise exercise);
+    }
+}
