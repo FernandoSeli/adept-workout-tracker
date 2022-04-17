1354cd7334366d1a5b094cff2e7ce1de43729f1b
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-04-17 20:30:38 +0200

    Added WorkoutTemplate Form #12

---
+++ src/Adept.Blazor/Services/IWorkoutTemplateService.cs
@@ -0,0 +1,10 @@
+﻿using Adept.Data.Model;
+
+namespace Adept.Blazor.Services
+{
+    public interface IWorkoutTemplateService
+    {
+        Task<int> AddWorkoutTemplateAsync(WorkoutTemplate workoutTemplate);
+        Task<int> UpdateWorkoutTemplateAsync(WorkoutTemplate workoutTemplate);
+    }
+}
---
+++ src/Adept.Blazor/Services/WorkoutTemplateService.cs
@@ -0,0 +1,29 @@
+﻿using Adept.Data;
+using Adept.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adept.Blazor.Services
+{
+    public class WorkoutTemplateService : IWorkoutTemplateService
+    {
+        private AdeptDatabaseContext _context;
+
+        public WorkoutTemplateService(IDbContextFactory<AdeptDatabaseContext> DbContextFactory)
+        {
+            _context = DbContextFactory.CreateDbContext();
+        }
+
+
+        public async Task<int> AddWorkoutTemplateAsync(WorkoutTemplate workoutTemplate)
+        {
+            await _context.WorkoutTemplates.AddAsync(workoutTemplate);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateWorkoutTemplateAsync(WorkoutTemplate workoutTemplate)
+        {
+            _context.WorkoutTemplates.Update(workoutTemplate);
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
