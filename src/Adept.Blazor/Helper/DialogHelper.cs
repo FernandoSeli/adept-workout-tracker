fbddbd11c10b915b901e37db6546c8f730800730
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-08-27 21:35:29 +0200

    Modifying routine and templates now uses dialogs

---
+++ src/Adept.Blazor/Helper/DialogHelper.cs
@@ -0,0 +1,49 @@
+﻿using Adept.Blazor.Shared.Dialogs;
+using Adept.Data.Model;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adept.Blazor.Helper
+{
+    public class DialogHelper
+    {
+        private IDialogService _dialogService;
+
+        public DialogHelper(IDialogService dialogService)
+        {
+            _dialogService = dialogService;
+        }
+
+        public async Task<Routine?> OpenRoutineDialogAsync(Routine routine)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("Routine", routine);
+            var dialog = _dialogService.Show<RoutineDialog>("", parameters);
+            var result = await dialog.Result;
+
+            if (!result.Cancelled)
+            {
+                return (Routine)result.Data;
+            }
+            return default;
+        }
+        public async Task<WorkoutTemplate?> OpenWorkoutTemplateDialogAsync(WorkoutTemplate workoutTemplate)
+        {
+            var parameters = new DialogParameters();
+            parameters.Add("WorkoutTemplate", workoutTemplate);
+            var dialog = _dialogService.Show<WorkoutTemplateDialog>("", parameters);
+            var result = await dialog.Result;
+
+            if (!result.Cancelled)
+            {
+                return (WorkoutTemplate)result.Data;
+            }
+            return default;
+        }
+    }
+}
