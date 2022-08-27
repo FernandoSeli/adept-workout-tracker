2107ecb99c5602523977a043089b9d254b34d3ce
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-08-27 21:35:02 +0200

    added list extension for adept.data

---
+++ src/Adept.Data/Extension/ListExtension.cs
@@ -0,0 +1,46 @@
+﻿using Adept.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adept.Data.Extension
+{
+    public static class ListExtension
+    {
+        public static void AddOrUpdateWorkoutTemplate(this List<WorkoutTemplate> workoutTemplates,
+            WorkoutTemplate newWorkoutTemplate)
+        {
+            if (newWorkoutTemplate != default)
+            {
+                var oldWorkoutTemplate = workoutTemplates.FirstOrDefault(x => x.Id == newWorkoutTemplate.Id);
+                if (oldWorkoutTemplate != default)
+                {
+                    oldWorkoutTemplate = newWorkoutTemplate;
+                }
+                else
+                {
+                    workoutTemplates.Add(newWorkoutTemplate);
+                }
+            }
+        }
+
+        public static void AddOrUpdateRoutine(this List<Routine> routines,
+            Routine newRoutine)
+        {
+            if (newRoutine != default)
+            {
+                var oldRoutine = routines.FirstOrDefault(x => x.Id == newRoutine.Id);
+                if (oldRoutine != default)
+                {
+                    oldRoutine = newRoutine;
+                }
+                else
+                {
+                    routines.Add(newRoutine);
+                }
+            }
+        }
+    }
+}
