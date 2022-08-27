3d295a79f2162d1e94b5dec77491a86ca2ab86b8
FernandoSeli <37588907+FernandoSeli@users.noreply.github.com>
2022-08-27 21:34:09 +0200

    Added Generic repository

---
+++ src/Adept.Data/Repository/GenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using Adept.Data;
+using Adept.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adept.Data.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private AdeptDatabaseContext _context;
+
+        public GenericRepository(IDbContextFactory<AdeptDatabaseContext> DbContextFactory)
+        {
+            _context = DbContextFactory.CreateDbContext();
+        }
+
+        public async Task<int> RemoveAsync(T test)
+        {
+            _context.Remove(test);
+            await _context.SaveChangesAsync();
+
+            _context.Entry(test).State = EntityState.Detached;
+            return 0;
+        }
+    }
+}
---
+++ src/Adept.Data/Repository/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿using Adept.Data;
+using Adept.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Adept.Data.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<int> RemoveAsync(T test);
+    }
+}
